0da50031a98d42aa5a11c29cccc967825ba55242
NadeesH <spnadeeshnimsara@gmail.com>
2024-01-02 13:25:46 +0530

    Add react context hook

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
+import { useWorkoutContext } from '../hooks/useWorkoutContext';
 
 const WorkoutForm = () => {
+  const { dispatch } = useWorkoutContext();
   const [title, setTitle] = useState('');
   const [reps, setReps] = useState('');
   const [load, setLoad] = useState('');
@@ -29,6 +31,7 @@ const WorkoutForm = () => {
       setLoad('');
       setError(null);
       console.log('new workout added', json);
+      dispatch({ type: 'CREATE_WORKOUT', payload: json });
     }
   };
 
